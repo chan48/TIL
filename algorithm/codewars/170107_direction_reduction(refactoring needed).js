3c7e96e87df0e147d9b70837ee0751c7e6cb54e0
joshua1988 <jangkeehyo@gmail.com>
2017-01-08 17:19:23 +0900

    took a note on what to implement in the next step to finalize the answer

--- algorithm/codewars/170107_direction_reduction(refactoring needed).js
+++ algorithm/codewars/170107_direction_reduction(refactoring needed).js
@@ -1,10 +1,14 @@
-var test1 = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"];  // ["WEST"]
-var test2 = ["NORTH", "EAST"];
-var test3 = ["NORTH", "WEST", "SOUTH", "EAST"];
-var test4 =
-['EAST','WEST','NORTH','SOUTH','NORTH','EAST','EAST','WEST','NORTH','SOUTH'];
+// var test1 = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"];  // ["WEST"]
+// var test2 = ["NORTH", "EAST"];
+// var test3 = ["NORTH", "WEST", "SOUTH", "EAST"];
+// var test4 =
+// ['EAST','WEST','NORTH','SOUTH','NORTH','EAST','EAST','WEST','NORTH','SOUTH'];
 var test5 =
 ['NORTH','SOUTH','WEST','EAST','NORTH','SOUTH','NORTH','EAST','WEST','EAST','SOUTH','NORTH','WEST', 'EAST'];
+
+
+// 바로 인접하지 않은 N-S W-E 쌍에 대해서는 줄이지 않는다를 구현
+var test3 = ["NORTH", "WEST", "SOUTH", "EAST"];
 
 function dirReduc(arr){
   var verFlag = false,
