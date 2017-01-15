9cecc7971aed87d51fc48f128132c15c4fd29c43
joshua1988 <jangkeehyo@gmail.com>
2017-01-15 23:37:32 +0900

    code wars snail sorting needs to be refactored

--- algorithm/codewars/170115_snail_sorting(refactoring needed).js
+++ algorithm/codewars/170115_snail_sorting(refactoring needed).js
@@ -11,10 +11,34 @@ snail([[1,2,3,4],[12,13,14,5],[11,16,15,6], [10,9,8,7]])
 [10,9 ,8 ,7]
 
 var snail = function (arr) {
-  var result = [];
+  var length = arr.length,
+      result = [];
 
-  
+  if (length == 1) return arr[0];
+  if (length == 0) return result;
 
+  for (var i = 0; i < length; i++) {
+    result.push(arr[0][i]);
+  }
+  for (var i = 1; i < length; i++) {
+    result.push(arr[i][length-1]);
+  }
+  for (var i = length-1; i > 0; i--) {
+    result.push(arr[length-1][i-1]);
+    // console.log("[length-1][i] : " + arr[length-1][i]);
+  }
+  // console.log("result : ", result); // [1, 2, 3, 4, 5, 6, 7]
+  for (var i = length-1; i > 1; i--) {
+    result.push(arr[i-1][0]);
+  }
+  console.log("result : ", result);
+
+  var subArray = [];
+  for (var i=1;i<length-1;i++) {
+    subArray.push(arr[i].splice(1,length-2));
+  }
+
+  result = result.concat(snail(subArray));
   return result;
 }
 snail([[1,2,3],[8,9,4],[7,6,5]]);
@@ -25,19 +49,19 @@ snail([[1,2,3,4],[12,13,14,5],[11,16,15,6], [10,9,8,7]]);
 arr[0][0]   arr[0][0]
 arr[0][1]   arr[0][1]
 arr[0][2]   arr[0][2]
-arr[1][2]   arr[0][3]
+arr[1][2]   arr[0][3] -
 arr[2][2]   arr[1][3]
 arr[2][1]   arr[2][3]
-arr[2][0]   arr[3][3]
+arr[2][0]   arr[3][3] -
 arr[1][0]   arr[3][2]
 arr[1][1]   arr[3][1]
-            arr[3][0]
+            arr[3][0] -
             arr[2][0]
-            arr[1][0]
+            arr[1][0] -
             arr[1][1]
-            arr[1][2]
-            arr[2][2]
-            arr[2][1]
+            arr[1][2] -
+            arr[2][2] -
+            arr[2][1] -
 
 
 
